9a16ec784ada301ddbdfd14ac78fe736d170e045
Konn <hello@konn.dev>
2023-05-22 21:10:20 +0900

    fast: utils.pyにparse_datetimeを実装

--- yaylib/utils.py
+++ yaylib/utils.py
@@ -1,3 +1,5 @@
-def parse_datetime(timestamp):
-    # TODO: タイムスタンプを日本時間に直して読めるようにする
-    return timestamp
+from datetime import datetime
+
+
+def parse_datetime(timestamp: int) -> str:
+    return str(datetime.fromtimestamp(timestamp))
