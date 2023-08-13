06591215cf1478647ac23f3588340dfaaca03dac
qualia-5w4 <geniusnaru@gmail.com>
2023-08-13 11:38:11 +0900

    login_with_emailを修正

--- yaylib/api/login.py
+++ yaylib/api/login.py
@@ -127,6 +127,7 @@ def login_with_email(
                 return LoginUserResponse(loaded_cookies)
 
             if secret_key is not None:
+                self.encrypt_cookie = True
                 self.secret_key = secret_key
                 self.fernet = Fernet(secret_key)
                 self.cookies = self.decrypt_cookies(self.fernet, loaded_cookies)
