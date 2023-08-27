07f8b22f8e08a80421a264ca3c9ddbd20b9557f2
qualia-5w4 <geniusnaru@gmail.com>
2023-08-27 21:11:15 +0900

    update follow new users for my bro

--- examples/follow_new_users.py
+++ examples/follow_new_users.py
@@ -7,11 +7,44 @@ password = "your_password"
 
 import yaylib
 
-api = yaylib.Client()
 
-api.login(email, password)
+class FollowBot:
+    def __init__(self, email=None, password=None):
+        self.api = yaylib.Client()
+        self.api.login(email, password)
 
-new_users = api.search_users(recently_created=True)
+    def run(self):
+        followed = 0
+        collected_ids = set()
 
-for new_user in new_users.users:
-    api.follow_user(new_user.id)
+        while True:
+            ids = []
+
+            self.api.logger.info("新規ユーザーを取得しています...")
+
+            new_users = self.api.search_users(recently_created=True, number=100)
+
+            new_ids = [
+                user.id
+                for user in new_users.users
+                if not user.is_following and user.id not in collected_ids
+            ]
+
+            ids.extend(new_ids)
+            collected_ids.update(new_ids)
+
+            self.api.logger.info(f"取得済みユーザー数: {len(ids)}")
+
+            for id in ids:
+                self.api.follow_user(id)
+
+            followed += len(ids)
+            self.api.logger.info(f"フォロー済み: {followed}")
+
+            ids.clear()
+            collected_ids.clear()
+
+
+if __name__ == "__main__":
+    bot = FollowBot(email, password)
+    bot.run()
