67673ec117a19a1155f7155d6a6359ced7cbae47
ekkx <geniusnaru@gmail.com>
2024-08-03 14:41:10 +0900

    fix: storage

--- yaylib/storage.py
+++ yaylib/storage.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2023 ekkx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import sqlite3
 
 from typing import Optional
@@ -6,6 +30,8 @@ from dataclasses import dataclass
 
 @dataclass(slots=True)
 class User:
+    """ストレージ内のユーザー型"""
+
     user_id: int
     email: str
     device_uuid: str
@@ -14,45 +40,52 @@ class User:
 
 
 class Storage:
-
-    instance = None
-
-    def __new__(cls):
-        if cls.instance is None:
-            cls.instance = super().__new__(cls)
-        return cls.instance
+    """`yaylib.Client` のステートを保存するローカルデータベースの操作を行う"""
 
     def __init__(self, path: str):
-        self.__con = sqlite3.connect(f"{path}.db")
+        self.path = path
+        self.__con = None
+        self.__cur = None
+
+    def __enter__(self):
+        self.__con = sqlite3.connect(self.path)
         self.__cur = self.__con.cursor()
         self.__cur.execute(
             """
-            CREATE TABLE IF NOT EXISTS "users" (
-            "id" INTEGER NOT NULL UNIQUE,
-            "email" TEXT NOT NULL,
-            "device_uuid" TEXT NOT NULL,
-            "access_token" TEXT NOT NULL,
-            "refresh_token" TEXT NOT NULL,
-            PRIMARY KEY("id"));
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                email TEXT NOT NULL,
+                device_uuid TEXT NOT NULL,
+                access_token TEXT NOT NULL,
+                refresh_token TEXT NOT NULL
+            );
             """
         )
         self.__con.commit()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__cur.close()
+        self.__con.close()
 
     def get_user(self, user_id: int) -> Optional[User]:
-        res = self.__cur.execute("SELECT * FROM users WHERE id = ?", (user_id))
-        result = res.fetchone()
-        if result is None:
-            return
-        else:
-            return User(
-                user_id=result[0],
-                email=result[1],
-                device_uuid=result[2],
-                access_token=result[3],
-                refresh_token=result[4],
-            )
+        """ユーザーを取得する"""
+        res = self.__cur.execute(f"SELECT * FROM users WHERE id = {user_id}")
+        user = res.fetchone()
+
+        if user is None:
+            return None
+
+        return User(
+            user_id=user[0],
+            email=user[1],
+            device_uuid=user[2],
+            access_token=user[3],
+            refresh_token=user[4],
+        )
 
     def create_user(self, user: User) -> bool:
+        """ユーザーを作成する"""
         try:
             self.__cur.execute(
                 "INSERT INTO users (id, email, device_uuid, access_token, refresh_token) VALUES (?, ?, ?, ?, ?)",
@@ -72,33 +105,39 @@ class Storage:
     def update_user(
         self,
         user_id: int,
-        email: Optional[str],
-        device_uuid: Optional[str],
-        access_token: Optional[str],
-        refresh_token: Optional[str],
+        email: Optional[str] = None,
+        device_uuid: Optional[str] = None,
+        access_token: Optional[str] = None,
+        refresh_token: Optional[str] = None,
     ) -> bool:
+        """ユーザーを更新する"""
         try:
             sql = "UPDATE users SET "
+            updates = []
+
             if email is not None:
-                sql += "email = ?"
+                updates.append(f"email = '{email}'")
             if device_uuid is not None:
-                sql += "device_uuid = ?"
+                updates.append(f"device_uuid = '{device_uuid}'")
             if access_token is not None:
-                sql += "access_token = ?"
+                updates.append(f"access_token = '{access_token}'")
             if refresh_token is not None:
-                sql += "refresh_token = ?"
-            sql += " WHERE id = ?"
-            self.__cur.execute(
-                sql, (email, device_uuid, access_token, refresh_token, user_id)
-            )
+                updates.append(f"refresh_token = '{refresh_token}'")
+
+            sql += ", ".join(updates)
+            sql += f" WHERE id = {user_id};"
+
+            self.__cur.execute(sql)
             self.__con.commit()
             return True
         except sqlite3.IntegrityError:
             return False
 
     def delete_user(self, user_id: int) -> bool:
+        """ユーザーを削除する"""
         try:
-            self.__cur("DELETE FROM users WHERE id = ? ", (user_id))
+            self.__cur.execute(f"DELETE FROM users WHERE id = {user_id} ")
+            self.__con.commit()
             return True
         except sqlite3.IntegrityError:
             return False
