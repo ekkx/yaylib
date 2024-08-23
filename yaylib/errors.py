726be42e95dd1ecf93ca87035e4f4ab6ae0e4ee9
ekkx <geniusnaru@gmail.com>
2024-08-23 16:03:30 +0900

    feat: add client custom errors

--- yaylib/errors.py
+++ yaylib/errors.py
@@ -24,211 +24,513 @@ SOFTWARE.
 
 import aiohttp
 
-__all__ = (
-    "YaylibError",
-    "HTTPError",
-    "BadRequestError",
-    "AuthenticationError",
-    "ForbiddenError",
-    "NotFoundError",
-    "RateLimitError",
-    "InternalServerError",
-    "ErrorCode",
-)
+from .responses import ErrorResponse
 
 
-class YaylibError(Exception):
-    """Base exception for yaylib"""
+class YaylibError(Exception):  # pylint: disable=used-before-assignment
+    """yaylib における例外基底クラス"""
 
 
-class HTTPError(YaylibError):
-    """HTTPError()
+class ClientError(YaylibError):
+    """yaylib クライアントに関する例外クラス"""
 
-    Exception raised when an HTTP request fails
-    """
+    def __init__(self, response: ErrorResponse):
+        super().__init__(f"Server returned error code: {response.error_code}")
 
-    def __init__(self, response: aiohttp.ClientResponse):
         self.__response = response
-        super().__init__(f"HTTP request failed with status: {response.status}")
 
     @property
-    def response(self) -> aiohttp.ClientResponse:
+    def response(self) -> ErrorResponse:
+        """エラーレスポンス"""
         return self.__response
 
 
-class BadRequestError(HTTPError):
-    """BadRequestError()
-
-    Exception raised for a 400 HTTP status code
-    """
+class UnknownError(ClientError):
+    """原因不明"""
 
 
-class AuthenticationError(HTTPError):
-    """AuthenticationError()
-
-    Exception raised for a 401 HTTP status code
-    """
+class InvalidParameterError(ClientError):
+    """パラメターが不正"""
 
 
-class ForbiddenError(HTTPError):
-    """ForbiddenError()
-
-    Exception raised for a 403 HTTP status code
-    """
+class RegisteredUserError(ClientError):
+    """このアカウントはすでに登録されています"""
 
 
-class NotFoundError(HTTPError):
-    """NotFoundError()
-
-    Exception raised for a 404 HTTP status code
-    """
+class AccessTokenExpiredError(ClientError):
+    """アクセストークンの有効期限切れ"""
 
 
-class RateLimitError(HTTPError):
-    """RateLimitError()
-
-    Exception raised for a 429 HTTP status code
-    """
+class ScreenNameAlreadyBeenTakenError(ClientError):
+    """このIDはすでに使われています"""
 
 
-class InternalServerError(HTTPError):
-    """InternalServerError()
-
-    Exception raised for a 5xx HTTP status code
-    """
+class UserNotFoundError(ClientError):
+    """ユーザーが見つかりません"""
 
 
-class AccessTokenExpiredError(AuthenticationError):
-    """AccessTokenExpiredError()
-
-    Exception raised when access token is invalid or expired
-    """
+class PostNotFoundError(ClientError):
+    """投稿が見つかりません"""
 
 
-class ErrorCode:
-    Unknown = 0
-    InvalidParameter = -1
-    RegisteredUser = -2
-    AccessTokenExpired = -3
-    ScreenNameAlreadyBeenTaken = -4
-    UserNotFound = -5
-    PostNotFound = -6
-    ChatRoomNotFound = -7
-    ChatMessageNotFound = -8
-    UserNotFoundAtChatRoom = -9
-    UserMustBeOverTwoAtChatRoom = -10
-    IncorrectPassword = -11
-    UserBlocked = -12
-    PrivateUser = -13
-    ApplicationNotFound = -14
-    BadSNSCredentials = -15
-    SNSAlreadyConnected = -16
-    CannotDisconnectSNS = -17
-    AccessTokenInvalid = -18
-    SpotNotFound = -19
-    UserBanned = -20
-    UserTemporaryBanned = -21
-    SchoolInfoChange = -22
-    CannotDeleteNewUser = -26
-    CaptchaRequired = -29
-    FailedToVerifyCaptcha = -30
-    Required2FA = -31
-    Incorrect2FA = -32
-    GroupIsFull = -100
-    BannedFromGroup = -103
-    InvalidCurrentPassword = -200
-    InvalidPassword = -201
-    InvalidEmailOrPassword = -202
-    ExistEmail = -203
-    BadEmailReputation = -204
-    ChatRoomIsFull = -308
-    ConferenceIsFull = -309
-    ConferenceInactive = -310
-    GroupOwnerBlockedYou = -312
-    ChatNeedMutualFollowed = -313
-    ConferenceCallIsLocked = -315
-    ConferenceCallIsForFollowersOnly = -317
-    InvalidEmail = -319
-    RegisteredEmail = -320
-    BannedFromCall = -321
-    NotCallOwner = -322
-    NotVipUser = -326
-    BlockingLimitExceeded = -331
-    VerificationCodeWrong = -332
-    VerificationCodeExpired = -333
-    InvalidAppVersion = -334
-    InvalidPhoneNumber = -335
-    FollowLimitation = -336
-    AgeGapNotAllowed = -338
-    GroupOwnerOrGroupModeratorOnly = -339
-    UnableToRegisterUserDueToPolicy = -340
-    SnsShareRewardAlreadyBeenClaimed = -342
-    QuotaLimitExceeded = -343
-    ChatNeedAgeVerified = -346
-    OnlyAgeVerifiedUserCanJoinGroup = -347
-    RequirePhoneVerificationToChat = -348
-    NotPostOwner = -350
-    GroupGenerationNotMatched = -352
-    PhoneNumberCheckVerificationCodeSubmitQuotaExceeded = -355
-    PhoneNumberCheckVerificationCodeRequestQuotaExceeded = -356
-    GroupOfferHasBeenAccepted = -357
-    GroupOfferHasBeenWithdrawn = -358
-    IpBanned = -360
-    NotConnectedToTwitter = -361
-    PrivateUserTimeline = -363
-    CounterRefreshLimitExceeded = -364
-    NotFollowedByOpponent = -367
-    ExceedChangeCountryQuota = -369
-    NotGroupMember = -370
-    GroupPendingTransfer = -371
-    GroupPendingDeputization = -372
-    UserRestrictedChatWithCautionUsers = -373
-    RestrictedCreateChatWithNewUsers = -374
-    RepostPostNotRepostable = -375
-    TooManyAccountsCreated = -376
-    OnlySpecificGenderCanJoinGroup = -377
-    CreateSpecificGenderGroupRequiredGender = -378
-    GroupRelatedExceededNumberOfRelatedGroups = -382
-    ExceededPinnedLimit = -383
-    GroupShareOnTwitterLimitExceeded = -384
-    ReportedContent = -385
-    InsufficientCoins = -400
-    ConferenceCallIsForMutualFollowsOnly = -402
-    ExceededLimit = -403
-    GroupInviteExceeded = -404
-    PhoneVerificationRequired = -405
-    ContentTooOld = -406
-    PasswordTooShort = -407
-    PasswordTooLong = -408
-    PasswordNotAllowed = -409
-    CommonPassword = -410
-    EmailNotAuthorized = -411
-    UnableToMovePostToThread = -412
-    UnableToPostUrl = -413
-    ReferralAlreadyRegistered = -415
-    MuteUserOverLimit = -416
-    InvalidAppVersion = -800
-    UnableToSetCall = -977
-    DynamicErrorMessage = -999
-    PhoneNumberBanned = -1000
-    BadRequest = 400
-    Unauthorized = 401
-    AccessForbidden = 403
-    NotFound = 404
-    Conflict = 409
-    TooManyRequests = 429
-    InternalServerError = 500
-    Web3AccountAlreadyLinkedToAnotherWallet = 4002
-    Web3WalletAlreadyLinkedToAnotherAccount = 4003
-    Web3PalLevelUpBattlesRequired = 4005
-    Web3PalLevelUpMaximumLevelReached = 4006
-    Web3PalPoolCooldown = 4010
-    Web3PalPoolEmpty = 4011
-    Web3PalAlreadyBattle = 4012
-    Web3WalletHasPendingTransactions = 4017
-    Web3WalletNetworkError = 5003
-    Web3EMPLInsufficientFunds = 6001
-    Web3EMPLFeeExceedsBalance = 6002
+class ChatRoomNotFoundError(ClientError):
+    """チャットルームが見つかりません"""
+
+
+class ChatMessageNotFoundError(ClientError):
+    """メッセージが見つかりません"""
+
+
+class UserNotFoundAtChatRoomError(ClientError):
+    """チャットルームに特定のユーザーが見つかりません"""
+
+
+class UserMustBeOverTwoAtChatRoomError(ClientError):
+    """チャットルーム内のユーザーは2人以上の必要があります"""
+
+
+class IncorrectPasswordError(ClientError):
+    """正しいパスワードを確認してください"""
+
+
+class UserBlockedError(ClientError):
+    """このユーザーはブロック中です"""
+
+
+class PrivateUserError(ClientError):
+    """プライベートユーザーです"""
+
+
+class ApplicationNotFoundError(ClientError):
+    """アプリケーションが見つかりません"""
+
+
+class BadSNSCredentialsError(ClientError):
+    """アカウント認証に失敗しました"""
+
+
+class SNSAlreadyConnectedError(ClientError):
+    """このSNSアカウントはすでに使われています"""
+
+
+class CannotDisconnectSNSError(ClientError):
+    """SNSの連携を解除できません"""
+
+
+class AccessTokenInvalidError(ClientError):
+    """アクセストークンが不正です"""
+
+
+class SpotNotFoundError(ClientError):
+    """スポットが見つかりません"""
+
+
+class UserBannedError(ClientError):
+    """このアカウントは永久的に凍結されました"""
+
+
+class UserTemporaryBannedError(ClientError):
+    """サービスの規約に抵触したため、アカウントを停止しています"""
+
+
+class SchoolInfoChangeError(ClientError):
+    """学校の情報が変更されています"""
+
+
+class CannotDeleteNewUserError(ClientError):
+    """アカウントを作成して3日以内に削除はできません"""
+
+
+class CaptchaRequiredError(ClientError):
+    """Captcha認証が必要です"""
+
+
+class FailedToVerifyCaptchaError(ClientError):
+    """Captcha認証に失敗しました"""
+
+
+class Required2FAError(ClientError):
+    """2段階認証が必要です"""
+
+
+class Incorrect2FAError(ClientError):
+    """2段階認証が不正です"""
+
+
+class GroupIsFullError(ClientError):
+    """サークルが満員です"""
+
+
+class BannedFromGroupError(ClientError):
+    """このサークルから追放されています"""
+
+
+class InvalidCurrentPasswordError(ClientError):
+    """現在のパスワードを入力してください"""
+
+
+class InvalidPasswordError(ClientError):
+    """現在のパスワードを確認してください"""
+
+
+class InvalidEmailOrPasswordError(ClientError):
+    """メールアドレス、もしくはパスワードが不正です"""
+
+
+class ExistEmailError(ClientError):
+    """このメールアドレスはすでに使われています"""
+
+
+class BadEmailReputationError(ClientError):
+    """スパムメールの可能性があり、操作を完了できません"""
+
+
+class ChatRoomIsFullError(ClientError):
+    """グループチャットの最大人数は50人です"""
+
+
+class ConferenceIsFullError(ClientError):
+    """通話が満員です"""
+
+
+class ConferenceInactiveError(ClientError):
+    """通話は終了しました"""
+
+
+class GroupOwnerBlockedYouError(ClientError):
+    """サークルの管理人にブロックされています"""
+
+
+class ChatNeedMutualFollowedError(ClientError):
+    """チャットするにはフォローされる必要があります"""
+
+
+class ConferenceCallIsLockedError(ClientError):
+    """通話はロックされています"""
+
+
+class ConferenceCallIsForFollowersOnlyError(ClientError):
+    """枠主をフォローすることで通話に参加できます"""
+
+
+class InvalidEmailError(ClientError):
+    """正しいメールアドレスを確認してください"""
+
+
+class RegisteredEmailError(ClientError):
+    """このメールアドレスはすでに登録されています"""
+
+
+class BannedFromCallError(ClientError):
+    """通話に参加できません。永久退出処分を受けています。"""
+
+
+class NotCallOwnerError(ClientError):
+    """通話のオーナーではありません"""
+
+
+class NotVipUserError(ClientError):
+    """VIPユーザーではありません"""
+
+
+class BlockingLimitExceededError(ClientError):
+    """ブロック数の上限に達しました"""
+
+
+class VerificationCodeWrongError(ClientError):
+    """認証コードが不正です"""
+
+
+class VerificationCodeExpiredError(ClientError):
+    """認証コードの有効期限切れ"""
+
+
+class InvalidPhoneNumberError(ClientError):
+    """この機能を使用するためには電話番号認証が必要です"""
+
+
+class FollowLimitationError(ClientError):
+    """制限に達したので、これ以上フォローすることができません。この制限はフォローワーを増やすことで、増やすことができます。"""
+
+
+class AgeGapNotAllowedError(ClientError):
+    """レター、もしくはチャットを送れません。年齢が離れすぎています"""
+
+
+class GroupOwnerOrGroupModeratorOnlyError(ClientError):
+    """サークル管理人か副管理人のみ可能な操作です"""
+
+
+class UnableToRegisterUserDueToPolicyError(ClientError):
+    """利用規約に基づき、現在アカウントを登録できません"""
+
+
+class SNSShareRewardAlreadyBeenClaimedError(ClientError):
+    """SNS共有の特典は既に取得済みです"""
+
+
+class QuotaLimitExceededError(ClientError):
+    """この機能の上限回数に達しました。1時間ほど時間を置いて再度お試しください。"""
+
+
+class ChatNeedAgeVerifiedError(ClientError):
+    """チャット送信に年齢確認が必要です"""
+
+
+class OnlyAgeVerifiedUserCanJoinGroupError(ClientError):
+    """このサークルに参加するには年齢確認をする必要があります"""
+
+
+class RequirePhoneVerificationToChatError(ClientError):
+    """チャットをするには電話番号認証が必要です"""
+
+
+class NotPostOwnerError(ClientError):
+    """編集は投稿の作成者のみ可能です"""
+
+
+class GroupGenerationNotMatchedError(ClientError):
+    """特定の年齢層のみ参加が許可されているサークルです"""
+
+
+class PhoneNumberCheckVerificationCodeSubmitQuotaExceededError(ClientError):
+    """SMS認証コードの送信回数が上限を越えました。1時間ほど時間をおいて再度お試しください"""
+
+
+class PhoneNumberCheckVerificationCodeRequestQuotaExceededError(ClientError):
+    """チャットをするには電話番号認証が必要です"""
+
+
+class GroupOfferHasBeenAcceptedError(ClientError):
+    """サークルの招待はすでに承諾されています"""
+
+
+class GroupOfferHasBeenWithdrawnError(ClientError):
+    """サークルの招待は拒否されています"""
+
+
+class IpBannedError(ClientError):
+    """IPアドレスが凍結されました"""
+
+
+class NotConnectedToTwitterError(ClientError):
+    """Twitterに接続されていません"""
+
+
+class PrivateUserTimelineError(ClientError):
+    """フォロワーにのみ投稿を公開しています"""
+
+
+class CounterRefreshLimitExceededError(ClientError):
+    """カウンター更新の回数制限に達しました"""
+
+
+class NotFollowedByOpponentError(ClientError):
+    """ユーザーがあなたをフォローする必要があります"""
+
+
+class ExceedChangeCountryQuotaError(ClientError):
+    """国設定は一度設定すると8時間は変更できません。時間をおいて再度お試しください"""
+
+
+class NotGroupMemberError(ClientError):
+    """サークルメンバーでないため、この通話に参加できません"""
+
+
+class GroupPendingTransferError(ClientError):
+    """グループ保留中の移動"""
+
+
+class GroupPendingDeputizationError(ClientError):
+    """グループ保留中の代理指名"""
+
+
+class UserRestrictedChatWithCautionUsersError(ClientError):
+    """相手は危険なユーザーとのチャットを許可していません"""
+
+
+class RestrictedCreateChatWithNewUsersError(ClientError):
+    """近日ペナルティなどを受けているため、新規のユーザーとのチャットルーム作成が制限されています"""
+
+
+class RepostPostNotRepostableError(ClientError):
+    """このユーザーの投稿は(´∀｀∩)↑age↑できません"""
+
+
+class TooManyAccountsCreatedError(ClientError):
+    """これ以上アカウントを作成することはできません"""
+
+
+class OnlySpecificGenderCanJoinGroupError(ClientError):
+    """特定の性別のみ参加が許可されているサークルです"""
+
+
+class CreateSpecificGenderGroupRequiredGenderError(ClientError):
+    """このサークルを作成するには性別の設定が必要です"""
+
+
+class GroupRelatedExceededNumberOfRelatedGroupsError(ClientError):
+    """サークルの追加上限に到達しました"""
+
+
+class ExceededPinnedLimitError(ClientError):
+    """これ以上ピン留めできません。解除してからもう一度お試しください"""
+
+
+class GroupShareOnTwitterLimitExceededError(ClientError):
+    """Twitterでのグループ共有の制限を超えています"""
+
+
+class ReportedContentError(ClientError):
+    """通報を受けているため、この投稿は(´∀｀∩)↑age↑できません"""
+
+
+class InsufficientCoinsError(ClientError):
+    """コインが不十分です"""
+
+
+class ConferenceCallIsForMutualFollowsOnlyError(ClientError):
+    """この通話に参加するには枠主と相互にフォローする必要があります"""
+
+
+class ExceededLimitError(ClientError):
+    """操作の上限に達しています"""
+
+
+class GroupInviteExceededError(ClientError):
+    """サークルにおける招待の許容数を超えました。時間をあけてから再度行ってください。"""
+
+
+class PhoneVerificationRequiredError(ClientError):
+    """参加するには電話番号認証が必要です。Yay! アプリから電話番号認証を行なってください。"""
+
+
+class ContentTooOldError(ClientError):
+    """一定期間の経過後、投稿の編集はできません"""
+
+
+class PasswordTooShortError(ClientError):
+    """パスワードが短すぎます"""
+
+
+class PasswordTooLongError(ClientError):
+    """パスワードが長すぎます"""
+
+
+class PasswordNotAllowedError(ClientError):
+    """他のパスワードを使用してください。無効な文字列が含まれています"""
+
+
+class CommonPasswordError(ClientError):
+    """他のパスワードを使用してください。文字列や数字、または記号などの組み合わせをお試しください"""
+
+
+class EmailNotAuthorizedError(ClientError):
+    """このメールアドレスを使用するには承認される必要があります"""
+
+
+class UnableToMovePostToThreadError(ClientError):
+    """投稿をスレッドに移動できません"""
+
+
+class UnableToPostUrlError(ClientError):
+    """URLを投稿できません"""
+
+
+class ReferralAlreadyRegisteredError(ClientError):
+    """description"""
+
+
+class MuteUserOverLimitError(ClientError):
+    """description"""
+
+
+class InvalidAppVersionError(ClientError):
+    """description"""
+
+
+class UnableToSetCallError(ClientError):
+    """通話を開始できませんでした"""
+
+
+class DynamicErrorMessageError(ClientError):
+    """description"""
+
+
+class PhoneNumberBannedError(ClientError):
+    """電話番号が凍結されています"""
+
+
+class BadRequestError(ClientError):
+    """リクエストが多すぎます"""
+
+
+class UnauthorizedError(ClientError):
+    """認証エラー"""
+
+
+class AccessForbiddenError(ClientError):
+    """アクセスが拒否されました"""
+
+
+class NotFoundError(ClientError):
+    """コンテンツは見つかりませんでした"""
+
+
+class ConflictError(ClientError):
+    """衝突が発生しました"""
+
+
+class TooManyRequestsError(ClientError):
+    """リクエストが多すぎます"""
+
+
+class InternalServerError(ClientError):
+    """サーバーエラー"""
+
+
+class Web3AccountAlreadyLinkedToAnotherWalletError(ClientError):
+    """この Web3 アカウントはすでに他のウォレットに連携されています"""
+
+
+class Web3WalletAlreadyLinkedToAnotherAccountError(ClientError):
+    """この Web3 アカウントはすでに他のアカウントに連携されています"""
+
+
+class Web3PalLevelUpBattlesRequiredError(ClientError):
+    """Web3PalLevelUpBattlesRequiredError"""
+
+
+class Web3PalLevelUpMaximumLevelReachedError(ClientError):
+    """Web3PalLevelUpMaximumLevelReachedError"""
+
+
+class Web3PalPoolCooldownError(ClientError):
+    """Web3PalPoolCooldownError"""
+
+
+class Web3PalPoolEmptyError(ClientError):
+    """Web3PalPoolEmptyError"""
+
+
+class Web3PalAlreadyBattleError(ClientError):
+    """Web3PalAlreadyBattleError"""
+
+
+class Web3WalletHasPendingTransactionsError(ClientError):
+    """Web3WalletHasPendingTransactionsError"""
+
+
+class Web3WalletNetworkErrorError(ClientError):
+    """Web3WalletNetworkErrorError"""
+
+
+class Web3EMPLInsufficientFundsError(ClientError):
+    """Web3EMPLInsufficientFundsError"""
+
+
+class Web3EMPLFeeExceedsBalanceError(ClientError):
+    """Web3EMPLFeeExceedsBalanceError"""
 
 
 class ErrorMessage:
