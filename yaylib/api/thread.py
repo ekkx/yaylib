00a61bcc2d8decd6ab7d1e3f9367d5a48d4bc89b
ekkx <geniusnaru@gmail.com>
2024-08-24 15:27:34 +0900

    doc: add thread api docstring

--- yaylib/api/thread.py
+++ yaylib/api/thread.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from typing import List, Optional
+from typing import List
 
 from .. import config
 from ..models import ThreadInfo
@@ -38,28 +38,52 @@ class ThreadApi:
         self.__client: Client = client
 
     async def add_post_to_thread(self, post_id: int, thread_id: int) -> ThreadInfo:
+        """投稿をスレッドに追加する
+
+        Args:
+            post_id (int):
+            thread_id (int):
+
+        Returns:
+            ThreadInfo:
+        """
         return await self.__client.request(
             "PUT",
             config.API_HOST + f"/v3/posts/{post_id}/move_to_thread/{thread_id}",
             return_type=ThreadInfo,
         )
 
-    async def convert_post_to_thread(
-        self,
-        post_id: int,
-        title: Optional[str] = None,
-        thread_icon_filename: Optional[str] = None,
-    ) -> ThreadInfo:
+    async def convert_post_to_thread(self, post_id: int, **params) -> ThreadInfo:
+        """投稿をスレッドに変換する
+
+        Args:
+            post_id (int):
+            title (str, optional):
+            thread_icon_filename (str, optional):
+
+        Returns:
+            ThreadInfo:
+        """
         return await self.__client.request(
             "POST",
             config.API_HOST + f"/v3/posts/{post_id}/move_to_thread",
-            json={"title": title, "thread_icon_filename": thread_icon_filename},
+            json=params,
             return_type=ThreadInfo,
         )
 
     async def create_thread(
         self, group_id: int, title: str, thread_icon_filename: str
     ) -> ThreadInfo:
+        """スレッドを作成する
+
+        Args:
+            group_id (int):
+            title (str):
+            thread_icon_filename (str):
+
+        Returns:
+            ThreadInfo:
+        """
         return await self.__client.request(
             "POST",
             config.API_HOST + "/v1/threads/",
@@ -71,22 +95,17 @@ class ThreadApi:
             return_type=ThreadInfo,
         )
 
-    async def get_group_thread_list(
-        self, group_id: int, from_str: Optional[str] = None, **params
-    ) -> GroupThreadListResponse:
+    async def get_group_thread_list(self, **params) -> GroupThreadListResponse:
+        """_summary_
+
+        Args:
+            group_id (int):
+            from_str (str, optional):
+            join_status (str, optional):
+
+        Returns:
+            GroupThreadListResponse:
         """
-
-        Parameters:
-        -----------
-
-            - group_id: int
-            - from_str: str = None
-            - join_status: str = None
-
-        """
-        params["group_id"] = group_id
-        if from_str:
-            params["from"] = from_str
         return await self.__client.request(
             "GET",
             config.API_HOST + "/v1/threads/",
@@ -95,6 +114,14 @@ class ThreadApi:
         )
 
     async def get_thread_joined_statuses(self, ids: List[int]) -> Response:
+        """スレッド参加ステータスを取得する
+
+        Args:
+            ids (List[int]):
+
+        Returns:
+            Response:
+        """
         return await self.__client.request(
             "GET",
             config.API_HOST + "/v1/threads/joined_statuses",
@@ -102,21 +129,18 @@ class ThreadApi:
             return_type=Response,
         )
 
-    async def get_thread_posts(
-        self, thread_id: int, from_str: Optional[str] = None, **params
-    ) -> PostsResponse:
+    async def get_thread_posts(self, thread_id: int, **params) -> PostsResponse:
+        """スレッド内のタイムラインを取得する
+
+        Args:
+            thread_id (int):
+            from_str (str, optional):
+            number (str, optional):
+
+        Returns:
+            PostsResponse:
         """
-
-        Parameters:
-        -----------
-
-            - post_type: str
-            - number: int = None
-            - from_str: str = None
-
-        """
-        if from_str:
-            params["from"] = from_str
+        params["from"] = params.get("from_str")
         return await self.__client.request(
             "GET",
             config.API_HOST + f"/v1/threads/{thread_id}/posts",
@@ -125,6 +149,15 @@ class ThreadApi:
         )
 
     async def join_thread(self, thread_id: int, user_id: int) -> Response:
+        """スレッドに参加する
+
+        Args:
+            thread_id (int):
+            user_id (int):
+
+        Returns:
+            Response:
+        """
         return await self.__client.request(
             "POST",
             config.API_HOST + f"/v1/threads/{thread_id}/members/{user_id}",
@@ -132,6 +165,15 @@ class ThreadApi:
         )
 
     async def leave_thread(self, thread_id: int, user_id: int) -> Response:
+        """スレッドから脱退する
+
+        Args:
+            thread_id (int):
+            user_id (int):
+
+        Returns:
+            Response:
+        """
         return await self.__client.request(
             "DELETE",
             config.API_HOST + f"/v1/threads/{thread_id}/members/{user_id}",
@@ -142,18 +184,34 @@ class ThreadApi:
         self,
         thread_id: int,
     ) -> Response:
+        """スレッドを削除する
+
+        Args:
+            thread_id (int):
+
+        Returns:
+            Response:
+        """
         return await self.__client.request(
             "DELETE",
             config.API_HOST + f"/v1/threads/{thread_id}",
             return_type=Response,
         )
 
-    async def update_thread(
-        self, thread_id: int, title: str, thread_icon_filename: str
-    ) -> Response:
+    async def update_thread(self, thread_id: int, **params) -> Response:
+        """スレッドをアップデートする
+
+        Args:
+            thread_id (int):
+            title (str):
+            thread_icon_filename (str):
+
+        Returns:
+            Response:
+        """
         return await self.__client.request(
             "PUT",
             config.API_HOST + f"/v1/threads/{thread_id}",
-            json={"title": title, "thread_icon_filename": thread_icon_filename},
+            json=params,
             return_type=Response,
         )
